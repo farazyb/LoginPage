849d512e84d7f935625c5544580514a46c919222
FYB <farazyazdanib@gmail.com>
2018-12-29 17:05:46 +0330

    Adding button

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button } from 'react-native'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -15,6 +16,7 @@ const instructions = Platform.select({
     'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -24,10 +26,13 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>hi how Are you!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+
       </View>
+
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
